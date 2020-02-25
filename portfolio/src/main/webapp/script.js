e2b26806ffb4306df7157dba79e5165ba8093914
Juan Torres <juanditb@gmail.com>
2020-02-25 17:43:29 -0500

    Finished step 2 of walkthrough. Site now has a button that fetches a message from the DataServlet class.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,25 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
-
-function addRandomFact() {
-    const facts = ['I\'m 19 years old.','My favorite color is blue.','I have a weiner dog named Tobi.',
-        'I grew up in South Florida.','My hobbies include gaming and weightlifting.'];
-
-    const randomFact = facts[Math.floor(Math.random() * facts.length)];
-    document.getElementById('fact-container').innerText = randomFact;
+async function getData() {
+    const response = await fetch('/data');
+    await console.log(response);
+    const data = await response.text();
+    document.getElementById('data-container').innerHTML = data;
 }
