55e4b3a9cd0b9c6a6bee3eeb3868f6be8d543121
Juan Torres <juanditb@gmail.com>
2020-03-12 11:32:50 -0400

    Implemented algorithm to find open meeting times.

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,93 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    // duration is longer than an entire day
+    if(request.getDuration() >= TimeRange.END_OF_DAY) {
+        return new ArrayList<TimeRange>();
+    }
+    
+    int lastEndTime = -1;
+    // put events into a list so we can sort and index them
+    ArrayList<Event> eventsList = new ArrayList<>();
+    for(Event e: events) {
+        eventsList.add(e);
+        lastEndTime = Math.max(lastEndTime,e.getWhen().end());
+    }
+
+    // comparator to sort events by start of their time range
+    Comparator<Event> comp = new Comparator<Event>() {
+        @Override
+        public int compare(Event a, Event b) {
+            return TimeRange.ORDER_BY_START.compare(a.getWhen(),b.getWhen());
+        }
+    };
+    
+    Collections.sort(eventsList,comp);
+
+    // list to store time ranges that we cannot hold meetings
+    List<TimeRange> unavailableTimes = new ArrayList<>();
+    for(int i = 0; i < eventsList.size(); i++) {
+        Event currEvent = eventsList.get(i);
+        boolean weGood = true;
+        for(String attendee: currEvent.getAttendees()) {
+            if(request.getAttendees().contains(attendee)) { 
+                weGood = false;
+                break;
+            }
+        }
+
+        if(weGood)
+            continue;
+        
+        int start = currEvent.getWhen().start();
+        int end = currEvent.getWhen().end();
+
+        if(i != events.size()-1) {
+            Event nextEvent = eventsList.get(i+1);
+            while(currEvent.getWhen().overlaps(nextEvent.getWhen())) {
+                end = Math.max(end,nextEvent.getWhen().end());
+                i++;
+                if(i == eventsList.size()-1)
+                    break;
+                nextEvent = eventsList.get(i+1);
+            }
+        }
+
+        unavailableTimes.add(TimeRange.fromStartEnd(start,end,false));
+    }
+
+    // list that holds the return value (open meeting times)
+    Collection<TimeRange> meetingTimes = new ArrayList<>();
+    
+    int start = 0;
+    // loops through unavailable times and sees if there are times inbetween them to hold the meeting
+    for(int i = 0; i < unavailableTimes.size(); i++) {
+        TimeRange uTime = unavailableTimes.get(i);
+        TimeRange meeting = TimeRange.fromStartEnd(start,uTime.start(),false);
+        if(meeting.duration() >= request.getDuration()) {
+            meetingTimes.add(meeting);
+        }
+        start = uTime.end();
+    }
+    // check last event time until the end of the day
+    if(unavailableTimes.size() > 0) {
+        TimeRange meeting = TimeRange.fromStartEnd(lastEndTime,TimeRange.END_OF_DAY,true);
+        if(meeting.duration() >= request.getDuration()) {
+            meetingTimes.add(meeting);
+        }
+    }
+    else {
+        // this means there are no unavailable times
+        meetingTimes.add(TimeRange.fromStartEnd(0,TimeRange.END_OF_DAY,true));
+    }
+
+    return meetingTimes;
   }
 }
